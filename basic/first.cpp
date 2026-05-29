b23b503fbceb0ca2ce00236e63609f7b6a65c4d5
Pratik Ghosh <pratikcse2006@gmail.com>
2026-05-29 12:06:08 +0530

    Modified the file first.cpp

--- basic/first.cpp
+++ basic/first.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
-
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+using namespace std;
+int main()
+{
+    cout << "My name is Pratik Ghosh" << endl;
+    cout << endl;
+    cout << "I am from a tier three college" << endl;
+    cout << "I am from West Bengal" << endl;
+    cout << "I am 18 years old" << endl;
+    cout << "I am a student of Computer Science and Engineering" << endl;
+    cout << "I am interested in programming and problem solving" << endl;
 }
